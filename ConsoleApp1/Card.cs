faf5c4a761ec4c63509e2f3b78709d3854b6b05e
Harsh Sharma <harsh.sharma@ceridian.com>
2022-09-19 10:41:32 -0400

    updates

--- ConsoleApp1/Card.cs
+++ ConsoleApp1/Card.cs
@@ -9,7 +9,19 @@ namespace Challenge1
     /// </summary>
     public class Card
     {
+        private string face;
+        private string suit;
 
+        public Card(string cardFace, string cardSuit)
+        {
+            face = cardFace;
+            suit = cardSuit;
 
+        }
+
+        public override string ToString()
+        {
+            return face + " of " + suit;
+        }
     }
 }
--- ConsoleApp1/PackOfCards.cs
+++ ConsoleApp1/PackOfCards.cs
@@ -15,17 +15,42 @@ namespace Challenge1
         /// </summary>
         /// <example>This shows the intended usage. <code>var cardFromTop = pack.Deal()</code></example>
         /// <returns>An instance of the <see cref="Card"/> class.</returns>
+        /// 
+        private Card[] packofcards;
+        private int currentCard;
+        private const int NUMBER_OF_CARDS = 52;
+        private Random randomNumber;
+
+        public PackOfCards()
+        {
+            string[] faces = { "Aces", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+            string[] suits = { "Hearts", "Clubs", "Diamonds", "Spades" };
+            packofcards = new Card[NUMBER_OF_CARDS];
+            currentCard = 0;
+            randomNumber = new Random();
+            for (int count = 0; count < packofcards.Length; count++)
+                packofcards[count] = new Card(faces[count % 11], suits[count / 13]);
+        }
         public Card Deal()
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+
+            if (currentCard < packofcards.Length)
+                return packofcards[currentCard++];
+            else
+                return null;
         }
+
 
         /// <summary>
         /// Returns the number of cards in the pack currently. <see cref="Deal"/> will decrement this number.  />
         /// </summary>
         public int CardsRemaining
         {
-            get { throw new NotImplementedException(); }
+            get
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
